834fd2f918db45a5b3bd4840d3924db64c416b05
Rohit Singh <rohitsinghemail@protonmail.com>
2022-02-05 21:31:47 +0530

    reg

--- src/Lib.hs
+++ src/Lib.hs
@@ -3,4 +3,13 @@ module Lib where
 type Top = Int
 type Size = Int
 type Array = [Int]
-data Stack = Stack Size Top Array  
+data Stack = Stack Size Top Array deriving Show
+
+newArray :: Size -> Stack
+newArray s = Stack s (-1) []
+
+push :: Stack -> Int -> Stack
+push (Stack size top arr) i = Stack size (top+1) (i : arr)
+
+pop :: Stack -> Stack
+pop (Stack size top (x:xs)) = Stack size (top-1) xs
