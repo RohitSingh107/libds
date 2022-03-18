82ea5e948c2e1b86bae6c88814ba124e2fdb3c7a
Rohit Singh <RohitSinghEmail@protonmail.com>
2022-03-18 19:42:49 +0530

    New Machine

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,16 +4,19 @@ type Top = Int
 type Size = Int
 type Array = [Int]
 
-data Stack = Stack Size Top Array deriving Show
+-- data Stack = Stack Size Top Array deriving Show
+-- 
+-- newArray :: Size -> Stack
+-- newArray s = Stack s (-1) []
+-- 
+-- push :: Stack -> Int -> Stack
+-- push (Stack size top arr) i = Stack size (top+1) (i : arr)
+-- 
+-- pop :: Stack -> Stack
+-- pop (Stack size top (x:xs)) = Stack size (top-1) xs
+-- 
 
-newArray :: Size -> Stack
-newArray s = Stack s (-1) []
-
-push :: Stack -> Int -> Stack
-push (Stack size top arr) i = Stack size (top+1) (i : arr)
-
-pop :: Stack -> Stack
-pop (Stack size top (x:xs)) = Stack size (top-1) xs
+data Stack a = Stack [a] deriving Show
 
 
 type Front = Int
